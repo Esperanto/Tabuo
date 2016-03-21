57f8b0d71f2eb121a5dc02cab5287bbc37817393
Georg Jaehnig <georg@jaehnig.org>
2016-03-22 00:11:52 +0100

    Eku eligi.

--- kreu.py
+++ kreu.py
@@ -1,10 +1,28 @@
 # -*- coding: utf-8 -*-
 
 import yaml
+import jinja2
 
 def sxargxu():
 
     kartoj = yaml.load(file('enhavo/kartoj.yml', 'r'))
-    print kartoj
+    return kartoj
 
-sxargxu()
+
+def eligu(kartoj):
+
+    env = jinja2.Environment()
+    env.trim_blocks = True
+    env.lstrip_blocks = True
+    env.loader=jinja2.FileSystemLoader('sxablonoj/')
+
+    rendered = env.get_template('kartoj.html').render(
+      kartoj = kartoj
+    )
+
+    with open('eligo.html', 'w') as f:
+        f.write(rendered.encode('utf-8'))
+
+
+kartoj = sxargxu()
+eligu(kartoj)
