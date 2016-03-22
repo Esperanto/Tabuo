d6302fb5f8d02b2d1aef29d79aad3be89bb66b16
Timwi <github@timwi.de>
2016-03-22 18:37:50 +0100

    korekti Esperanton

--- kreu.py
+++ kreu.py
@@ -10,8 +10,8 @@ for i in range(0, len(kartoj)):
     kartoj[i]['indekso'] = i + 1
 
 
-# Kreu printebla HTML enhavanta po 9 kartojn por paĝo.
-def eligu_9_po_pagxo(kartoj):
+# Kreu printeblan HTML-on enhavantan po 9 kartojn por paĝo.
+def eligu_9_por_pagxo(kartoj):
 
     # kreu liston, en kiu la anoj estas grupitaj per specifa nombro
     def grupigu(listo, nombro):
@@ -34,28 +34,28 @@ def eligu_9_po_pagxo(kartoj):
     env.lstrip_blocks = True
     env.loader = jinja2.FileSystemLoader('sxablonoj/')
 
-    rendered = env.get_template('9_po_pagxo.html').render(
+    rendered = env.get_template('9_por_pagxo.html').render(
         pagxoj = pagxoj
     )
 
     with open('eligo/9_po_pagxo.html', 'w') as f:
         f.write(rendered.encode('utf-8'))
 
-# Kreu printebla HTML enhavanta po 1 kartojn por paĝo.
-# (Eblas decidi ĉe la print-dialogo, kiom da kartoj oni volas havi po printita paĝo.)
-def eligu_1_po_pagxo(kartoj):
+# Kreu printeblan HTML-on enhavantan po 1 karton por paĝo.
+# (Eblas decidi ĉe la print-dialogo, kiom da kartoj oni volas havi sur ĉiu printita paĝo.)
+def eligu_1_por_pagxo(kartoj):
 
     env = jinja2.Environment()
     env.trim_blocks = True
     env.lstrip_blocks = True
     env.loader = jinja2.FileSystemLoader('sxablonoj/')
 
-    rendered = env.get_template('1_po_pagxo.html').render(
+    rendered = env.get_template('1_por_pagxo.html').render(
         kartoj = kartoj
     )
 
     with open('eligo/1_po_pagxo.html', 'w') as f:
         f.write(rendered.encode('utf-8'))
 
-eligu_1_po_pagxo(kartoj)
-eligu_9_po_pagxo(kartoj)
+eligu_1_por_pagxo(kartoj)
+eligu_9_por_pagxo(kartoj)
