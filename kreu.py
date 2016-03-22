ce80f178b05262967d915c4b6cceefd439669ab0
Georg Jaehnig <georg@jaehnig.org>
2016-03-22 02:35:58 +0100

    Kreu unu karto per pagxo, por printi kun multaj kartoj/pagxoj sur unu en la retumilo.

--- kreu.py
+++ kreu.py
@@ -23,6 +23,21 @@ def eligu(kartoj):
     with open('eligo.html', 'w') as f:
         f.write(rendered.encode('utf-8'))
 
+def eligu_a4(kartoj):
+
+    env = jinja2.Environment()
+    env.trim_blocks = True
+    env.lstrip_blocks = True
+    env.loader=jinja2.FileSystemLoader('sxablonoj/')
+
+    rendered = env.get_template('a4.html').render(
+      kartoj = kartoj
+    )
+
+    with open('eligo.html', 'w') as f:
+        f.write(rendered.encode('utf-8'))
+
 
 kartoj = sxargxu()
-eligu(kartoj)
+#eligu(kartoj)
+eligu_a4(kartoj)
