13fd18c83a69398b1b9e2e595f95b5143b3502d3
Timwi <github@timwi.de>
2016-03-22 14:42:54 +0100

    Repari HTML-kreadon (kaj purigi dosierojn)

--- kreu.py
+++ kreu.py
@@ -1,43 +1,60 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 
 import yaml
 import jinja2
 
-def sxargxu():
+# ŝarĝu kartojn
+kartoj = yaml.load(file('enhavo/kartoj.yml', 'r'))
+# aldonu la indeksojn
+for i in range(0, len(kartoj)):
+    kartoj[i]['indekso'] = i + 1
 
-    kartoj = yaml.load(file('enhavo/kartoj.yml', 'r'))
-    return kartoj
 
+# kreu 'kartoj.html'-dosieron (printebla HTML enhavanta po 9 kartojn por paĝo)
+def eligu_html(kartoj):
 
-def eligu(kartoj):
+    # kreu liston, en kiu la anoj estas grupitaj per specifa nombro
+    def grupigu(listo, nombro):
+        grupoj = []
+        i = 0;
+        while i < len(listo):
+            sublisto = []
+            for j in range(0, nombro):
+                if i < len(listo):
+                    sublisto.append(listo[i])
+                i += 1
+            grupoj.append(sublisto)
+        return grupoj
+
+    linioj = grupigu(kartoj, 3)
+    pagxoj = grupigu(linioj, 3)
 
     env = jinja2.Environment()
     env.trim_blocks = True
     env.lstrip_blocks = True
-    env.loader=jinja2.FileSystemLoader('sxablonoj/')
+    env.loader = jinja2.FileSystemLoader('sxablonoj/')
 
     rendered = env.get_template('kartoj.html').render(
-      kartoj = kartoj
+        pagxoj = pagxoj
     )
 
-    with open('eligo.html', 'w') as f:
+    with open('eligo/kartoj.html', 'w') as f:
         f.write(rendered.encode('utf-8'))
 
+# kreu 'a4.html'-dosieron (ĉiu karto estas unu A4-paĝo)
 def eligu_a4(kartoj):
 
     env = jinja2.Environment()
     env.trim_blocks = True
     env.lstrip_blocks = True
-    env.loader=jinja2.FileSystemLoader('sxablonoj/')
+    env.loader = jinja2.FileSystemLoader('sxablonoj/')
 
     rendered = env.get_template('a4.html').render(
-      kartoj = kartoj
+        kartoj = kartoj
     )
 
-    with open('eligo.html', 'w') as f:
+    with open('eligo/a4.html', 'w') as f:
         f.write(rendered.encode('utf-8'))
 
-
-kartoj = sxargxu()
-#eligu(kartoj)
+eligu_html(kartoj)
 eligu_a4(kartoj)
